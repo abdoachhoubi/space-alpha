9577e7617718ec00b24241c8212d0dc831c5d4a5
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-04-20 23:24:32 +0100

    Minor change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
