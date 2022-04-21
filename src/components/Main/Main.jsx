d40561363acab5d019e7b413f2bce2d6ba2e6564
abdoachhoubi <abdo.achhoubi2@gmail.com>
2022-04-21 06:14:52 +0100

    Added a loading animation

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { Minus } from "react-feather";
 import Card from "./Card";
 
 const Main = () => {
-  let [posts, setPosts] = useState(<h2>loading</h2>);
+  let [posts, setPosts] = useState(
+    <main>
+      <div className="loader">
+        <Minus color="#FFFFFF" size={30} />
+      </div>
+    </main>
+  );
   let [limit, setLimit] = useState(30);
+  let [btn, setBtn] = useState("main-btn");
+  let [loader, setLoader] = useState("hidden");
 
   let getNews = () => {
     async function getData() {
@@ -12,6 +21,8 @@ const Main = () => {
       );
       const data = await response.json();
       setPosts(data.map((item) => <Card key={item.id} data={item} />));
+      setLoader("hidden");
+      setBtn("main-btn");
     }
     getData();
   };
@@ -19,15 +30,20 @@ const Main = () => {
   useEffect(getNews, [limit]);
 
   const showMore = () => {
+    setBtn("hidden");
+    setLoader("loader");
     setLimit((prevLimit) => prevLimit + 9);
   };
   return (
     <main>
       <h1 className="main-heading">Latest Space News</h1>
       <div className="main">{posts}</div>
-      <button className="main-btn" onClick={showMore}>
+      <button className={btn} onClick={showMore}>
         Show More
       </button>
+      <div className={loader}>
+        <Minus color="#FFFFFF" size={30} />
+      </div>
     </main>
   );
 };
